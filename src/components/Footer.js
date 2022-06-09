6f771da344ef80291a473fa964f65bb00efc6b8a
Provashish Roy <provashishroy95@gmail.com>
2022-06-10 02:04:58 +0600

    footer section completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,47 +3,21 @@ import React from 'react';
 const Footer = () => {
     let date = new Date().getFullYear();
     return (
-        <footer className='footer-style py-5 footer-img'>
-            <div className="container ">
-                <div className="row g-3 justify-content-between py-5  ">
-                    <div className="col-sm-12 col-md-4 col-lg-4 ">
-                        <div className='footer-text'>
-                            <h6>ABOUT US</h6>
-                            <p>Books warehouse that stores books & stationary items in a warehouse knows that effective management is the difference between fulfilling customer orders quickly and accurately while keeping costs down versus hearing complaints about delayed or inaccurate shipments and higher operating costs.</p>
-                            <span><i className="fa-solid fa-envelope "></i> bookswarehouse@gmail.com</span>
-                            <p><i className="fa-solid fa-phone"></i> 01632544810</p>
-                        </div>
-                    </div>
+        <footer className='py-5 footer-bg'>
 
-                    <div className="col-sm-12 col-md-4 col-lg-4 ">
-                        <div className='footer-text'>
-                            <h6>ADDRESS</h6>
-                            <p>Books Warehouse, Sukrabad-102 .Rose View Plaza (7th Floor, Room-808)
-                                185 Elephant Road, Hatirpool
-                                Dhaka-1205, Bangladesh</p>
-                        </div>
-                    </div>
-                    <div className="col-sm-12 col-md-4 col-lg-4 ">
-                        <div className='footer-text'>
-                            <h6 className='footer-text mx-3'>USEFUL LINK</h6>
-                            <span className='mt-4'>
-                                <i className="fa-brands fa-github icon-style"></i>
-                                <i className="fa-brands fa-facebook icon-style"></i>
-                                <i className="fa-brands fa-google icon-style"></i>
-                                <i className="fa-brands fa-whatsapp icon-style"></i>
-                                <i className="fa-brands fa-twitter icon-style"></i>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <hr className='footer-text' />
-                <div className="row ">
-                    <div className='col-12 '>
-                        <div className="text-center footer-text">
-                            &copy; Copyright {date} | Books Warehouse by provashish
-                        </div>
-                    </div>
-                </div>
+            <div className='social-link text-center fs-1 mt-5'>
+                <a href="https://www.linkedin.com/in/provashish/" target="_blank" className='me-2'>
+                    <span><i className="fa-brands fa-linkedin text-color"></i></span>
+                </a>
+                <a href="https://github.com/provashish95" target="_blank" className='me-2'>
+                    <span><i className="fa-brands fa-github text-color"></i></span>
+                </a>
+                <a className="mailto" href="mailto:provashishroy95@gmail.com">
+                    <span><i className="fa-solid fa-envelope text-color"></i></span>
+                </a>
+            </div>
+            <div className="text-center mb-5">
+                <p style={{ color: '#437c8f' }}>&copy; Copyright {date} | Provashish Roy</p>
             </div>
 
         </footer>
