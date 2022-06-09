e738dcc2ffa01e765b3ea6d39fd4495db4d1ccc4
Provashish Roy <provashishroy95@gmail.com>
2022-06-09 17:33:10 +0600

    navbar completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
 
 const Header = () => {
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-style py-3  navbar-light ">
             <div className="container">
-                <a className="navbar-brand" href="#">Provashish</a>
+                <Link className="navbar-brand fs-3 fw-bold" to="/">
+                    PROVASHISH
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarText">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" href="#">Home</a>
+                            <Link className="nav-link fw-bold fs-5 text-white" to="#">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
+                            <Link className="nav-link fw-bold fs-5 text-white" to="#">About</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Project</a>
+                            <Link className="nav-link fw-bold fs-5 text-white" to="#">Project</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link fw-bold fs-5 text-white" to="#">Download resume</Link>
                         </li>
                     </ul>
-                    <span className="navbar-text">
-                        Download resume
-                    </span>
+
                 </div>
             </div>
         </nav>
