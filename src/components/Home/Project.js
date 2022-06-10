5712043192629bde70ba771a0f0beb5f417bab51
Provashish Roy <provashishroy95@gmail.com>
2022-06-10 21:52:48 +0600

    completed projects section

--- src/components/Home/Project.js
+++ src/components/Home/Project.js
@@ -3,34 +3,37 @@ import { useNavigate } from 'react-router-dom';
 
 const Project = ({ project }) => {
     const navigate = useNavigate();
-    const { id, name, img, description, live } = project;
+
+    const { id, name, img1, description, live } = project;
 
     const navigateToPurchase = (id) => {
         if (id === 1) {
-            navigate(`/projectDetails`);
+            navigate('/projectDetails');
             return;
         } else if (id === 2) {
-            navigate(`/projectDetails1`);
+            navigate('/projectDetails1');
             return;
         } else {
-            navigate(`/projectDetails2`);
+            navigate('/projectDetails2');
         }
 
     }
     return (
-        <div className="col-sm-12 col-md-4 col-lg-4">
-            <div className="card shadow-lg">
-                <img src={img} className="card-img-top" alt="..." />
+
+        <div className="col-sm-12 col-md-4 col-lg-4 my-3">
+            <div className="card shadow-lg white-img">
+                <img src={img1} className="card-img-top" alt="img" style={{ height: '180px' }} />
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
-                    <p className="card-text">{description.slice(0, 100)}...</p>
+                    <p className="card-text">{description.slice(0, 110)}...</p>
                     <p className='text-center'>
                         <a href={live} className="project-live-btn" target='_blank'>Live website</a>
-                        <button onClick={() => navigateToPurchase(id)} className="project-details-btn">Details</button>
+                        <button onClick={() => navigateToPurchase(id)} className='project-details-btn'>Details</button>
                     </p>
                 </div>
             </div>
         </div>
+
     );
 };
 
