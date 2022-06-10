f5686de2cb61e1d5a7099d0c9abb484217870d35
Provashish Roy <provashishroy95@gmail.com>
2022-06-10 22:04:58 +0600

    spinner added

--- src/components/Home/Loading.js
+++ src/components/Home/Loading.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Spinner } from 'react-bootstrap';
 
 const Loading = () => {
     return (
         <div style={{ height: '200px' }} className='w-100 d-flex align-items-center justify-content-center'>
-            <Spinner animation="border" variant="info" />
+            <div className="spinner-border text-info" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
         </div>
     );
 };
